ca8c07877145d2f74fa65a97d2c684a976ea459e
Nishant Gada <49140662+NishantGada@users.noreply.github.com>
2021-04-08 18:47:43 +0530

    Add files via upload

--- cricket.py
+++ cricket.py
@@ -6,14 +6,22 @@ balls = 0
 overs = 0
 wickets = 0
 extras = 0
+score = 0
+
+
+class Extras:
+    leg_byes = 0
+    wide = 0
+    no_ball = 0
 
 
 class NewBatsman:
     runs = 0
     balls = 0
     out = False
-    if balls != 0:
-        strike_rate = runs / balls
+    # strike_rate = 0
+    # if balls != 0:
+    #     strike_rate = runs / balls
 
     def __init__(self, name):
         self.name = name
@@ -35,8 +43,7 @@ non_strike = NewBatsman(batsmen[wickets + 1])
 while 1:
     if balls > 0 and balls % 6 == 0:
         strike, non_strike = non_strike, strike
-        overs += 1
-        # print("overs = ", overs)
+        # overs += 1
     print()
     print(strike.name, "*")
     print(strike.runs, "/", strike.balls)
@@ -44,9 +51,12 @@ while 1:
     print(non_strike.name)
     print(non_strike.runs, "/", non_strike.balls)
     print()
+    print("Score = ", score)
+    # print()
     print("Total balls = ", balls)
     if balls > 0 and balls % 6 == 0:
-        print("overs = ", overs)
+        overs = balls / 6
+        print("Overs = ", overs)
     print()
     print("==========================================")
     print()
@@ -54,9 +64,11 @@ while 1:
     print("Press 2 for wicket")
     print("Press 3 for dot ball")
     print("Press 4 for extras")
+    print("Press 5 for batsman statistics")
+    # print("Press 4 for extras")
     print()
 
-    choice = int(input("Next Ball = "))
+    choice = int(input("Enter choice = "))
     balls += 1
     print()
 
@@ -69,6 +81,7 @@ while 1:
 
         else:
             print()
+            score += runs
             strike.runs += runs
             strike.balls += 1
 
@@ -111,6 +124,32 @@ while 1:
     elif choice == 3:
         strike.balls += 1
 
+    elif choice == 4:
+        print("Press 1 for Wide")
+        print("Press 2 for Leg Byes")
+        print("Press 3 for No Ball")
 
+        print()
+        extra = int(input("Enter choice = "))
 
+        if extra == 1:
+            Extras.wide += 1
+            print("Wide = ", Extras.wide)
+
+        elif extra == 2:
+            Extras.leg_byes += 1
+            print("Leg Byes = ", Extras.leg_byes)
+
+        elif extra == 3:
+            Extras.no_ball += 1
+            print("No Ball = ", Extras.no_ball)
+
+        extras += 1
+        balls -= 1
+        score += 1
+        print("Extras = ", extras)
+
+    elif choice == 5:
+        print(strike.name, " ---> ", (strike.runs / strike.balls) * 100)
+        print(non_strike.name, " ---> ", (non_strike.runs / non_strike.balls) * 100)
 
